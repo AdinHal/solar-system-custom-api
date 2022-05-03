554c44803c3797784d1093c4a25184397234fd27
Adin Halilovic <adin.halilovic15@gmail.com>
2022-05-03 12:21:54 +0200

    imageUrl

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const planets = [
       "satelites": 0,
       "surfaceArea": "6.078.747.774.547 km2",
       "rotationPeriod": "25.38 d",
-      "imgUrl": "https://www.dropbox.com/s/delbe2cjgn47p40/Saturn.png?dl=1"
+      "imageUrl": "https://www.dropbox.com/s/delbe2cjgn47p40/Saturn.png?dl=1"
     },
     {
         "name": "Mercury",
@@ -21,7 +21,7 @@ const planets = [
         "satelites": 0,
         "surfaceArea": "74.800.000 km2",
         "rotationPeriod": "176 d",
-        "imgUrl": "https://www.dropbox.com/s/w6hz4o8atxnl8sl/Mercury.png?dl=1"
+        "imageUrl": "https://www.dropbox.com/s/w6hz4o8atxnl8sl/Mercury.png?dl=1"
     },
     {
         "name": "Venus",
@@ -29,7 +29,7 @@ const planets = [
         "satelites": 0,
         "surfaceArea": "460.200.000 km2",
         "rotationPeriod": "-116.75 d",
-        "imgUrl": "https://www.dropbox.com/s/o3g8g8zq21yraez/Uranus.png?dl=1"
+        "imageUrl": "https://www.dropbox.com/s/o3g8g8zq21yraez/Uranus.png?dl=1"
     },
     {
         "name": "Earth",
@@ -37,7 +37,7 @@ const planets = [
         "satelites": 1,
         "surfaceArea": "510.100.000 km2",
         "rotationPeriod": "1.0 d",
-        "imgUrl": "https://www.dropbox.com/s/hlj9k3gbj8vbwne/Earth.png?dl=1"
+        "imageUrl": "https://www.dropbox.com/s/hlj9k3gbj8vbwne/Earth.png?dl=1"
     },
     {
         "name": "Mars",
@@ -45,7 +45,7 @@ const planets = [
         "satelites": 2,
         "surfaceArea": "144.800.000 km2",
         "rotationPeriod": "1.02749125 d",
-        "imgUrl": ""
+        "imageUrl": "https://www.dropbox.com/s/4uqyp9rfylevcv0/Mars.png?dl=1"
     },
     {
         "name": "Jupiter",
@@ -53,7 +53,7 @@ const planets = [
         "satelites": 80,
         "surfaceArea": "61.420.000.000 km2",
         "rotationPeriod": "9.9258 h",
-        "imgUrl": "https://www.dropbox.com/s/rnpalgdr3gi5kdr/Jupiter.png?dl=1"
+        "imageUrl": "https://www.dropbox.com/s/rnpalgdr3gi5kdr/Jupiter.png?dl=1"
     },
     {
         "name": "Saturn",
@@ -61,7 +61,7 @@ const planets = [
         "satelites": 83,
         "surfaceArea": "42.700.000.000 km2",
         "rotationPeriod": "10h 32m 36s",
-        "imgUrl": "https://www.dropbox.com/s/delbe2cjgn47p40/Saturn.png?dl=1"
+        "imageUrl": "https://www.dropbox.com/s/delbe2cjgn47p40/Saturn.png?dl=1"
     },
     {
         "name": "Uranus",
@@ -69,7 +69,7 @@ const planets = [
         "satelites": 27,
         "surfaceArea": "8.083.000.000 km2",
         "rotationPeriod": "-0.71832 d",
-        "imgUrl": "https://www.dropbox.com/s/o3g8g8zq21yraez/Uranus.png?dl=1"
+        "imageUrl": "https://www.dropbox.com/s/o3g8g8zq21yraez/Uranus.png?dl=1"
     },
     {
         "name": "Neptune",
@@ -77,7 +77,7 @@ const planets = [
         "satelites": 14,
         "surfaceArea": "7.618.000.000 km2",
         "rotationPeriod": "0.67125 d",
-        "imgUrl": "https://www.dropbox.com/s/5gahey8nf49oxn1/Neptune.png?dl=1"
+        "imageUrl": "https://www.dropbox.com/s/5gahey8nf49oxn1/Neptune.png?dl=1"
     }
   ];
 app.get("/", (req,res)=>{
